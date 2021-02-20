4b9915a01400ac21016e209d14582e72055aadf0
Jose Guillermo <jguillermo@outlook.com>
2021-02-20 11:54:28 -0500

    test properties

--- ddd-cli/src/modules/load-data/application/read-yaml/process-propertie.service.spec.ts
+++ ddd-cli/src/modules/load-data/application/read-yaml/process-propertie.service.spec.ts
@@ -4,8 +4,9 @@ import { DataSkeleton } from '../../domain/DataSkeleton';
 
 describe('Process Propertie', () => {
   let service: ReadYamlService;
+  let processData: DataSkeleton[];
 
-  function processPathData(pathFolder):DataSkeleton[]{
+  function processPathData(pathFolder): DataSkeleton[] {
     const relativePath = `${service.relativePath()}/templates/config/test/properties/${pathFolder}`;
     const files = service.getFiles(relativePath);
     const data = service.readFiles(files, relativePath);
@@ -21,12 +22,8 @@ describe('Process Propertie', () => {
   });
 
   describe('processData one entity', () => {
-    let processData: DataSkeleton[];
-    beforeEach(() => {
-      processData = processPathData('/one_entity');
-    });
-
     it('process default value properties', () => {
+      processData = processPathData('/one_entity');
       expect(processData[0].properties[0].name.value).toEqual('aggregate');
       expect(processData[0].properties[0].params[0].json).toEqual({
         name: 'id',
@@ -39,6 +36,38 @@ describe('Process Propertie', () => {
         required: false,
         type: 'string',
         defaultValue: 'JJ',
+      });
+    });
+  });
+
+  describe('processData two entitys', () => {
+    it('process value properties', () => {
+      processData = processPathData('/two_entity');
+      expect(processData[0].properties[0].name.value).toEqual('aggregate');
+      expect(processData[0].properties[0].params[0].json).toEqual({
+        name: 'id',
+        required: false,
+        type: 'id',
+        defaultValue: null,
+      });
+      expect(processData[0].properties[0].params[1].json).toEqual({
+        name: 'name',
+        required: false,
+        type: 'string',
+        defaultValue: 'JJ',
+      });
+      expect(processData[1].properties[0].name.value).toEqual('aggregate');
+      expect(processData[1].properties[0].params[0].json).toEqual({
+        name: 'id',
+        required: false,
+        type: 'id',
+        defaultValue: null,
+      });
+      expect(processData[1].properties[0].params[1].json).toEqual({
+        name: 'name',
+        required: false,
+        type: 'string',
+        defaultValue: 'BB',
       });
     });
   });
